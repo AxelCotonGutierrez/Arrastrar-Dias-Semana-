0684e35f39488896762c67b3f8e79d3926e3ac26
AxelCotonGutierrez <learningmathematicas@gmail.com>
2024-09-26 12:41:45 +0200

    nav

--- script.js
+++ script.js
@@ -179,13 +179,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Navegaciòn"  
- 
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menu = document.querySelector('.menu');
-        
-    menuToggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
-    });
-  });
